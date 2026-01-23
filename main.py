ae11b94304825b061c6a59d1bfe56823e5ed2d83
MarkLindstorff <MLAGit@protonmail.com>
2026-01-23 19:37:26 +0100

    ticks added

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    
+    dt = 0
+
 
     while True:
         log_state()
@@ -16,6 +23,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
